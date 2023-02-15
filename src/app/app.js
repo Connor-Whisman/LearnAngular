0e5e06eca1546af75380065fe03f3084ba9bd0f6
Connor Whisman <connorwhisman17@gmail.com>
2023-02-15 13:03:56 -0500

    Finish routing views

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,23 @@
-var appMain = angular.module('app', ['ngMessages', 'ngRoute']); // Use array to declare outside dependencies like ngMessages
+var app = angular.module('app', ['ngMessages', 'ngRoute']); // Use array to declare outside dependencies like ngMessages
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/main', {
+        templateUrl: 'templates/main.html',
+        controller: 'appController'
+    })
+    .when('/new', {
+        templateUrl: 'templates/new.html',
+        controller: 'newController'
+    })
+})
 
 // $scope is an angular service. Uses dependency injection to pass dynamic data between model and view
 // $log is another service with features to log data in a clean way. Instead of console.log
 
 // Best way to write dependency injection. Minifying will not break because angular will look
 // at the strings to see what should be injected into the functions variables. Order of array elements matters
-appMain.controller('appController', ['$scope', '$log', function($scope, $log) {
+app.controller('appController', ['$scope', '$log', function($scope, $log) {
 
     $scope.default = "Injection v2";
     $scope.logData = function() {
@@ -18,7 +30,7 @@ appMain.controller('appController', ['$scope', '$log', function($scope, $log) {
 }]);
 
 
-appMain.controller('httpController', ['$scope', '$log', '$http', function($scope, $log, $http) {    
+app.controller('httpController', ['$scope', '$log', '$http', function($scope, $log, $http) {    
     const URL   = 'http://localhost:8080';
 
     $http.get(URL)
@@ -40,19 +52,8 @@ appMain.controller('httpController', ['$scope', '$log', '$http', function($scope
 
 }]);
 
-appMain.controller('newController', ['$scope', '$log', function($scope, $log) {    
-    $scope.test = 'hello new'
+app.controller('newController', ['$scope', '$log', function($scope, $log) {    
+    $scope.test = 'hello new';
 }]);
 
 
-appMain.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl: 'index.html',
-        controller: 'appController'
-    })
-    .when('/new', {
-        templateUrl: 'new.html',
-        controller: 'newController'
-    })
-})
