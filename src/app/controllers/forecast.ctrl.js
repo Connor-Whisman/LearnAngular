bb17cb5cfec9b2da580f6d371c94cfd110bacc68
Connor <connorwhisman17@gmail.com>
2023-03-08 21:39:23 -0500

    Coordinates from city state country, then weather

--- src/app/controllers/forecast.ctrl.js
+++ src/app/controllers/forecast.ctrl.js
@@ -15,33 +15,51 @@ weather.controller('forecastCtrl', [
         $scope.apiKey   = locationSvc.apiKey;
 
 
-        $scope.geoAPI =
+        var geoAPI =
             $resource(`http://api.openweathermap.org/geo/1.0/direct`, 
                 {query: {method: 'get', isArray: true}}
             );
-        $scope.geoResult = $scope.geoAPI.query(
+        var geoResult = geoAPI.query(
             {
                 q: (`${$scope.city},${$scope.state},${$scope.country}`),
                 limit: 1,
                 appid: $scope.apiKey
             }
-        );
+        )
         
-        console.log($scope.geoResult);
 
-        // $scope.weatherAPI =
-        //     $resource(`https://api.openweathermap.org/data/2.5/weather`, 
-        //         {callback: 'JSON_CALLBACK'},
-        //         {get: {method: 'JSONP'}}
-        //     );
+        var weatherAPI =
+            $resource(`https://api.openweathermap.org/data/2.5/weather`, 
+                {callback: 'JSON_CALLBACK'},
+                {get: {method: 'JSONP'}}
+            );
 
-        // $scope.weatherResult = $scope.weatherAPI.get(
+        // var weatherResult = weatherAPI.get(
         //     {
-        //         lat: $scope.geoResult.lat,
-        //         lon: $scope.geoResult.lon,
+        //         lat: $scope.geoResult[0].lat,
+        //         lon: $scope.geoResult[0].lon,
         //         appid: $scope.apiKey
         //     }
         // );
+
+        geoResult.$promise.then(function() {
+            var lat = geoResult[0].lat;
+            var lon = geoResult[0].lon;
+
+            var weatherResult = weatherAPI.get(
+                {
+                    lat: lat,
+                    lon: lon,
+                    appid: $scope.apiKey
+                }
+            );
+
+            $scope.weatherResult = weatherResult;
+
+            console.log(weatherResult);
+        });
+        
+        
 
         $scope.convert = function(kelvin) {
             return ((kelvin - 273.15) * 1.8 + 32).toFixed(1)
